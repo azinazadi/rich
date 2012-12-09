4a5d6021bce0592b40dd50eecd6a738141799d9d
Keith Pops <keith.poplawski@gmail.com>
2012-12-08 23:58:39 -0500

    hope this fixes the query :pray:

--- app/controllers/rich/files_controller.rb
+++ app/controllers/rich/files_controller.rb
@@ -11,10 +11,10 @@ module Rich
       if(params[:scoped] == 'true')
         if(@type == "image")
           # @items = RichFile.images.order("created_at DESC").where("owner_type = ? AND owner_id = ?", params[:scope_type], params[:scope_id]).page params[:page]
-          @items = RichFile.images.desc(:created_at).and({owner_type: params[:scope_type]}, {owner_id: params[:scope_id]}).page params[:page]
+          @items = RichFile.images.desc(:created_at).where(owner_type: params[:scope_type]).and(owner_id: params[:scope_id]).page params[:page]
         else
           # @items = RichFile.files.order("created_at DESC").where("owner_type = ? AND owner_id = ?", params[:scope_type], params[:scope_id]).page params[:page]
-          @items = RichFile.files.desc(:created_at).and({owner_type: params[:scope_type]}, {owner_id: params[:scope_id]}).page params[:page]
+          @items = RichFile.files.desc(:created_at).where(owner_type: params[:scope_type]).and(owner_id: params[:scope_id]).page params[:page]
         end        
       else
         if(@type == "image")
